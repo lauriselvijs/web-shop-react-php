60ab292dabdafb42b2f0ee5eb8e57e7810093439
Lauris <lauriselvijsm@gmail.com>
2022-01-24 18:55:53 +0200

    added checkbox fix

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ProductItem from "./ProductItem";
 import "../styles/css/product-list.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -8,8 +7,9 @@ export class ProductList extends Component {
   constructor(props) {
     super(props);
 
-    this.setIdArray = this.setIdArray.bind(this);
     this.onDelete = this.onDelete.bind(this);
+    this.onChecked = this.onChecked.bind(this);
+    this.checkRef = React.createRef();
 
     this.state = {
       products: [],
@@ -27,29 +27,35 @@ export class ProductList extends Component {
         products: [...this.state.products, ...productData.data.data],
       });
     } catch (error) {
-      console.log(error);
-
+      this.setState({
+        products: [],
+      });
       return error;
     }
   }
 
-  setIdArray(id) {
-    this.setState({ idArray: [...this.state.idArray, id] });
-  }
-
   onDelete() {
-    this.state.idArray.map(async (id) => {
+    const checkboxes = document.getElementsByClassName("delete-checkbox");
+    let checkboxArray = [];
+    for (let i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked === true) {
+        console.log(checkboxes[i].name);
+        checkboxArray.push(checkboxes[i].name);
+      }
+    }
+
+    checkboxArray.map(async (id) => {
       try {
         await axios.delete(
           "http://localhost/react-php-test/api/products/delete.php",
           { data: { id } }
         );
-
-        this.setState({
-          products: this.state.products.filter(
-            (products) => products.id !== id
-          ),
-        });
+        window.location.reload();
+        // this.setState({
+        //   products: this.state.products.filter(
+        //     (products) => products.id !== id
+        //   ),
+        // });
       } catch (error) {
         console.log(error);
         return error;
@@ -57,8 +63,17 @@ export class ProductList extends Component {
     });
   }
 
+  onChecked(id, event) {
+    //console.log(event.target.checked, event.target.name);
+    // this.setState({
+    //  idArray: [...this.state.idArray, event.target.name],
+    // });
+  }
+
   render() {
-    const { products } = this.state;
+    const { products, idArray } = this.state;
+
+    console.log(idArray);
 
     return (
       <>
@@ -78,11 +93,28 @@ export class ProductList extends Component {
         <hr className="line-break-header" />
         <div className="grid-container">
           {products.map((product, index) => (
-            <ProductItem
-              key={index}
-              product={product}
-              onDelete={this.setIdArray.bind(this, product.id)}
-            />
+            <div className="product-item" key={index}>
+              <div>{product.sku}</div>
+              <div>{product.name}</div>
+              <div>{product.price} $</div>
+              <div>
+                {product.type.toLowerCase() === "dvd" && (
+                  <>Size: {product.product_attribute} MB</>
+                )}
+                {product.type.toLowerCase() === "book" && (
+                  <>Weight: {product.product_attribute} KG</>
+                )}
+                {product.type.toLowerCase() === "furniture" && (
+                  <>Dimension: {product.product_attribute}</>
+                )}
+              </div>
+              <input
+                type="checkbox"
+                name={product.id}
+                className="delete-checkbox"
+                onChange={this.onChecked.bind(this, product.id)}
+              />
+            </div>
           ))}
         </div>
       </>
